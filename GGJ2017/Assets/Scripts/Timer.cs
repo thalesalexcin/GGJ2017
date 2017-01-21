d21469e43100a90d3988b0f8d1805f200dc0ed22
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2017-01-21 16:47:19 +0100

    Timer

---
+++ GGJ2017/Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour {
+
+    private Text timerUI;
+    [HideInInspector]
+    public float currentTime;
+    private bool isRunning;
+
+	// Use this for initialization
+	void Start () {
+        timerUI = GetComponent<Text>();
+        Reset();
+        StartTimer();
+    }
+
+    public void Reset()
+    {
+        currentTime = 0;
+    }
+
+    public void StartTimer()
+    {
+        isRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isRunning)
+        {
+            currentTime += Time.deltaTime;
+        }
+        timerUI.text = currentTime.ToString("F2");
+	}
+}
