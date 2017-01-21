3eb3eb857f6965c52c48785bdb774e8d0d8b00c7
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2017-01-21 18:48:09 +0100

    Des trucs de menus

--- GGJ2017/Assets/Scripts/EndPoint.cs
+++ GGJ2017/Assets/Scripts/EndPoint.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndPoint : MonoBehaviour {
 
@@ -15,4 +16,9 @@ public class EndPoint : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void LoadLevel (string level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
 }
